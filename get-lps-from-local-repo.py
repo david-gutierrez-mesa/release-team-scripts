92e7efcd5ebe73e0648a9fba8a400da87246cd30
david-gutierrez-mesa <david.gutierrez@liferay.com>
2024-01-19 10:00:11 +0100

    RTS-0001 Get reverted list

--- get-lps-from-local-repo.py
+++ get-lps-from-local-repo.py
@@ -12,13 +12,20 @@ def main(repo_path, start_hash, end_hash):
 
     individual_commit_hashes = of_interest.split('\n')
     lps_list = []
+    revered_list = []
 
     for commit_hash in individual_commit_hashes:
-        message = liferay_portal_ee_repo.commit(commit_hash).message.split(' ')[0].split('\n')[0]
-        if (message not in lps_list) and (message.startswith('LPS-')):
-            lps_list.append(message)
+        message = liferay_portal_ee_repo.commit(commit_hash).message
+        lps = message.split(' ')[0].split('\n')[0]
+        if message.lower().find('revert') != -1:
+            revered_list.append('https://github.com/liferay/liferay-portal-ee/commit/' + commit_hash)
+        elif (lps not in lps_list) and (lps.startswith('LPS-')):
+            lps_list.append(lps)
 
+    print(" List of LPS:")
     print(*lps_list, sep="\n")
+    print("\n\n List of reverted LPS:")
+    print(*revered_list, sep="\n")
 
 
 if __name__ == '__main__':
